0e5a4751c8237d9d25c120b86a1273559db861d3
surge <david.mitchell@digital-shokunin.net>
2014-12-04 16:11:57 -0500

    Custom style sheet

--- web/app.py
+++ web/app.py
@@ -37,6 +37,11 @@ def makeresponse():
 def redir():
     return redirect('/')
 
+@app.route('/css/<cssfile>')
+def css(cssfile):
+    response = render_template('css/' + cssfile)
+    return response
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
